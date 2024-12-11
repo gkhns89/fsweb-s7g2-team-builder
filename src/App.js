9da62ac56ba2c69b123faa6e521314c900101010
Gökhan <gokhansisman@outlook.com>
2024-12-11 05:26:08 +0300

    Form created and submit button function added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     { name: "Ali", favDay: "Salı", favFood: "Et" },
     { name: "Selin", favDay: "Cuma", favFood: "Soğan" },
   ]);
-  
 
   return (
     <div className="App">
@@ -17,18 +16,36 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
           {memberList.map((member, index) => (
-            <li key={index}><strong>{member.name}</strong> - {member.favDay} - {member.favFood}</li>
+            <li key={index}>
+              <strong>{member.name}</strong> - {member.favDay} -{" "}
+              {member.favFood}
+            </li>
           ))}
         </ul>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const name = e.target.name.value;
+            const favDay = e.target.favDay.value;
+            const favFood = e.target.favFood.value;
+            setMemberList([...memberList, { name, favDay, favFood }]);
+            e.target.reset();
+          }}
         >
-          Learn React
-        </a>
+          <input type="text" name="name" placeholder="Name" /><br />
+          <select name="favDay" id="favDay" defaultValue="Pazartesi"><br />
+            <option value="Pazartesi">Pazartesi</option>
+            <option value="Salı">Salı</option>
+            <option value="Çarşamba">Çarşamba</option>
+            <option value="Perşembe">Perşembe</option>
+            <option value="Cuma">Cuma</option>
+            <option value="Cumartesi">Cumartesi</option>
+            <option value="Pazar">Pazar</option>
+          </select><br />
+          <input type="text" name="favFood" placeholder="Fav Food" /><br />
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
