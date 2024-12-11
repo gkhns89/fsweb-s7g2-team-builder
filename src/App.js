4b2dad53b344ed6ec18fd8c757278ab011ec3ebb
Gökhan <gokhansisman@outlook.com>
2024-12-11 05:13:01 +0300

    Create state elements and list them

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [memberList, setMemberList] = useState([
+    { name: "Mehmet", favDay: "Pazar", favFood: "Pide" },
+    { name: "Aysenur", favDay: "Pazartesi", favFood: "Tavuk" },
+    { name: "Ali", favDay: "Salı", favFood: "Et" },
+    { name: "Selin", favDay: "Cuma", favFood: "Soğan" },
+  ]);
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {memberList.map((member, index) => (
+            <li key={index}><strong>{member.name}</strong> - {member.favDay} - {member.favFood}</li>
+          ))}
+        </ul>
+
         <a
           className="App-link"
           href="https://reactjs.org"
